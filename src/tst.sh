e00b917ca01942ce8f0f9ad33ae436ff83a341ca
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-02-29 14:38:44 +0000

    oesp. belongs to 0.12.12
    git-svn-id: https://ilk.uvt.nl/svn/sources/Frog/trunk@14358 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/tst.sh
+++ src/tst.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 ./frog --skip=p -t $srcdir/../tests/tst.txt -c $srcdir/../config/small-frog.cfg -o tst.out >& /dev/null
-diff tst.out $srcdir/../tests/tst.ok
+diff -w tst.out $srcdir/../tests/tst.ok
