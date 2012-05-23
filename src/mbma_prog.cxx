3535f530db149fd0a576d66b5dff0d47495f195e
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-05-23 15:41:49 +0000

    added a simple mbma main program. it just gives al Morphological variants. No POS tags involved!
    git-svn-id: https://ilk.uvt.nl/svn/sources/Frog/trunk@14793 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ src/mbma_prog.cxx
@@ -0,0 +1,190 @@
+/*
+  $Id:$
+  $URL:$
+
+  Copyright (c) 2006 - 2012
+  Tilburg University
+
+  A Morphological-Analyzer for Dutch
+ 
+  This file is part of frog
+
+  frog is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  frog is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      http://ilk.uvt.nl/software.html
+  or send mail to:
+      timbl@uvt.nl
+*/
+
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+#include "timbl/TimblAPI.h"
+#include "timbl/LogStream.h"
+#include "timblserver/TimblServerAPI.h"
+
+// individual module headers
+
+#include "config.h"
+#include "frog/Configuration.h"
+#include "libfolia/folia.h"
+#include "frog/mbma_mod.h"
+
+using namespace std;
+using namespace Timbl;
+
+LogStream my_default_log( cerr, "mbma", StampMessage ); // fall-back
+LogStream *theErrLog = &my_default_log;  // fill the externals
+
+string TestFileName;
+string ProgName;
+int tpDebug = 0; //0 for none, more for more output
+
+Configuration configuration;
+static string configDir = string(SYSCONF_PATH) + "/" + PACKAGE + "/";
+static string configFileName = configDir + "frog.cfg";
+
+void usage( ) {
+  cout << endl << "Options:\n";
+  cout << "\t============= INPUT MODE (mandatory, choose one) ========================\n"
+       << "\t -t <testfile>          Run frog on this file\n"
+       << "\t -c <filename>    Set configuration file (default " << configFileName << ")\n"
+       << "\t============= OTHER OPTIONS ============================================\n"
+       << "\t -h. give some help.\n"
+       << "\t -V or --version .   Show version info.\n"
+       << "\t -d <debug level>    (for more verbosity)\n";
+}
+
+static Mbma myMbma;
+
+bool parse_args( TimblOpts& Opts ) {
+  string value;
+  bool mood;
+  if ( Opts.Find('V', value, mood ) ||
+       Opts.Find("version", value, mood ) ){
+    // we already did show what we wanted.
+    exit( EXIT_SUCCESS );
+  }
+  if ( Opts.Find ('h', value, mood)) {
+    usage();
+    exit( EXIT_SUCCESS );
+  };
+  // is a config file specified?
+  if ( Opts.Find( 'c',  value, mood ) ) {
+    configFileName = value;
+    Opts.Delete( 'c' );
+  };
+
+  if ( configuration.fill( configFileName ) ){
+    cerr << "config read from: " << configFileName << endl;
+  }
+  else {
+    cerr << "failed te read configuration from! '" << configFileName << "'" << endl;
+    cerr << "did you correctly install the frogdata package?" << endl;
+    return false;
+  }
+
+  // debug opts
+  if ( Opts.Find ('d', value, mood)) {
+    if ( !Timbl::stringTo<int>( value, tpDebug ) ){
+      cerr << "-d value should be an integer" << endl;
+      return false;
+    }
+    Opts.Delete('d');
+  };
+
+  if ( Opts.Find( 't', value, mood )) {
+    TestFileName = value;
+    ifstream is( value.c_str() );
+    if ( !is ){
+      cerr << "input stream " << value << " is not readable" << endl;
+      return false;
+    }
+    Opts.Delete('t');
+  };
+  return true;
+}
+
+bool init(){  
+  // for some modules init can take a long time
+  // so first make sure it will not fail on some trivialities
+  //
+  if ( !myMbma.init( configuration ) ){
+    cerr << "Initialization failed." << endl;
+    return false;
+  }
+  cerr << "Initialization done." << endl;
+  return true;
+}
+
+void Test( istream& in ){
+  string line;
+  while ( getline( in, line ) ){
+    vector<string> words;
+    split_at_first_of( line, words, " \t\"',./;':<>?!`~()@#$%^&*-_=+[]{}\\|" );
+    for ( size_t w=0; w < words.size(); ++w ){
+      vector<vector<string> > res = myMbma.analyze( words[w] );
+      cout << words[w] << "\t";
+      for ( size_t i=0; i < res.size(); ++i ){
+	cout << res[i];
+	if ( i < res.size()-1 )
+	  cout << "/";
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+  return;
+}
+
+
+int main(int argc, char *argv[]) {
+  std::ios_base::sync_with_stdio(false);
+  cerr << "mbma " << VERSION << " (c) ILK 1998 - 2012" << endl;
+  cerr << "Induction of Linguistic Knowledge Research Group, Tilburg University" << endl;
+  ProgName = argv[0];
+  cerr << "based on [" << Timbl::VersionName() << "]" << endl;
+  cout << "configdir: " << configDir << endl;
+  try {
+    TimblOpts Opts(argc, argv);
+    if ( parse_args(Opts) ){
+      if (  !init() ){
+	cerr << "terminated." << endl;
+	return EXIT_FAILURE;
+      }
+      ifstream in(TestFileName.c_str() );
+      if ( in.good() ){
+	Test( in );
+      }
+      else {
+	cerr << "unable to open: " << TestFileName << endl;
+	return EXIT_FAILURE;
+      }
+    }
+    else {
+      return EXIT_FAILURE;
+    }
+  }
+  catch ( const exception& e ){
+    cerr << "fatal error: " << e.what() << endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
