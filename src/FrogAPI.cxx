e467414c2dfe6d17554efbf2cedba58c94e5e756
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2014-09-15 15:56:42 +0000

    Copyright text added
    git-svn-id: https://ilk.uvt.nl/svn/sources/Frog/trunk@17664 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/FrogAPI.cxx
+++ src/FrogAPI.cxx
@@ -1,4 +1,34 @@
 /* ex: set tabstop=8 expandtab: */
+/*
+  $Id: Frog.cxx 17655 2014-09-15 13:03:20Z sloot $
+  $URL: https://ilk.uvt.nl/svn/sources/Frog/trunk/src/Frog.cxx $
+
+  Copyright (c) 2006 - 2014
+  Tilburg University
+
+  A Tagger-Lemmatizer-Morphological-Analyzer-Dependency-Parser for Dutch
+
+  This file is part of frog
+
+  frog is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  frog is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      http://ilk.uvt.nl/software.html
+  or send mail to:
+      timbl@uvt.nl
+*/
+
 // Python.h seems to best included first. It tramples upon defines like:
 // _XOPEN_SOURCE, _POSIX_C_SOURCE" etc.
 #include "Python.h"
