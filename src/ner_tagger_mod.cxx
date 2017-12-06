6c76df800ae31b1491d1b6fe819b2b7487a85d99
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2017-12-06 16:49:05 +0100

    finalized ner-override code

--- src/ner_tagger_mod.cxx
+++ src/ner_tagger_mod.cxx
@@ -437,8 +437,6 @@ string to_tag( const string& label, bool inside ){
 
 void NERTagger::post_process( const vector<folia::Word*>& swords,
 			      const vector<string>& override ){
-  using TiCC::operator<<;
-  cerr << "override = " << override << endl;
   vector<string> tags;
   vector<double> conf;
   for ( const auto& tag : _tag_result ){
@@ -446,24 +444,47 @@ void NERTagger::post_process( const vector<folia::Word*>& swords,
     conf.push_back( tag.confidence() );
   }
   if ( !override.empty() ){
+    using TiCC::operator<<;
+    cerr << "override = " << override << endl;
+    cerr << "ner tags = " << tags << endl;
     bool inside = false;
     string label;
     for ( size_t i=0; i < tags.size(); ++i ){
-      if ( tags[i] == "O" ) {
-	// may be overridden
-	if ( override[i] != "O" ){
-	  inside = (label == override[i] );
-	  // cerr << "REPLACE " << tags[i] << " by "
-	  //      << to_tag(override[i], inside ) << endl;
-	  tags[i] = to_tag(override[i], inside );
-	  if ( !inside ){
-	    label = override[i];
+      if ( override[i] != "O" ){
+	// there is something to override
+	if ( tags[i][0] == 'I' && !inside ){
+	  //oops, inside a NER tag, and now a new override
+	  // whipe previous I tags, and one B
+	  for ( size_t j = i-1; j > 0; --j ){
+	    if ( tags[j][0] == 'B' ){
+	      // we are done
+	      tags[j] = "O";
+	      break;
+	    }
+	    tags[j] = "O";
+	  }
+	  // whipe next I tags too, to be sure
+	  for ( size_t j = i+1; j < tags.size(); ++j ){
+	    if ( tags[j][0] != 'I' ){
+	      // a B or O
+	      break;
+	    }
+	    // still inside
+	    tags[j] = "O";
 	  }
 	}
-	else {
-	  inside = false;
-	  label = "";
+	inside = (label == override[i] );
+	// cerr << "REPLACE " << tags[i] << " by "
+	//      << to_tag(override[i], inside ) << endl;
+	tags[i] = to_tag(override[i], inside );
+	conf[i] = 1;
+	if ( !inside ){
+	  label = override[i];
 	}
+      }
+      else {
+	inside = false;
+	label = "";
       }
     }
   }
