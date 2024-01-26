802354eebf47f2039518dbcdd0200563ecccb40d
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2024-01-26 15:17:54 +0100

    entering 2024

--- include/frog/clex.h
+++ include/frog/clex.h
@@ -1,6 +1,6 @@
 /* ex: set tabstop=8 expandtab: */
 /*
-  Copyright (c) 2006 - 2023
+  Copyright (c) 2006 - 2024
   CLST  - Radboud University
   ILK   - Tilburg University
 
--- include/frog/ucto_tokenizer_mod.h
+++ include/frog/ucto_tokenizer_mod.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2006 - 2023
+  Copyright (c) 2006 - 2024
   CLST  - Radboud University
   ILK   - Tilburg University
 
